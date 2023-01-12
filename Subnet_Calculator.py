75596c7d2e4e2975e58297e8ca9d33c49a5c51a8
srt <sharat.nautiyal@gmail.com>
2023-01-12 16:42:57 +0800

    added the print of the network block

--- Subnet_Calculator.py
+++ Subnet_Calculator.py
@@ -1,8 +1,10 @@
-
 import math
 import re
-cidr = "172.28.31.22/26"
+cidr = "10.10.31.22/25"
 new= cidr.split("/")
+octets1=new[0]
+octets2=octets1.split(".")
+print(octets2)
 
 class_A= re.search("10.*.*,*",new[0])
 class_B=re.search("(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)", new[0])
@@ -18,12 +20,12 @@ subnets= int(new[1])-block
 hosts= 32-int(new[1])
 print(hosts, subnets)
 #test=new[1]/8 !=0 then div = test.split(".") and power = new[1]-(8*div), z2^power is th
-test=int(new[1])/8
-print(test)
-if test !=0:
-    test=str(test)
-    div = test.split(".")
-    power1=int(div[0])
+octet_used1=int(new[1])/8
+print(octet_used1)
+if octet_used1 !=0:
+    octet_used2=str(octet_used1)
+    final_octet= octet_used2.split(".")
+    power1=int(final_octet[0])
     print(power1)
     power2=int(new[1])-(8*power1)
 print(power2)
@@ -31,5 +33,38 @@ print(power2)
 power3=8-power2
 
 network=math.pow(2,power3)
+network=int(network)
 print(network)
 
+if class_B:
+    octet_B=[]
+
+    for i in range(power1-1):
+        octet_B.append(octets2[i])
+
+    IP = ".".join(octet_B)
+    print(IP)
+
+    print(f"The network block size is {network}; range is {IP}.{network}.0 - {IP}.{network}.255 ")
+
+if class_A:
+    octet_A=[]
+
+    for i in range(power1-1):
+        octet_A.append(octets2[i])
+
+    IP = ".".join(octet_A)
+    print(IP)
+
+    print(f"The network block size is {network}; range is {IP}.{network}.0 - {IP}.{network}.255 ")
+
+if class_C:
+    octet_C=[]
+
+    for i in range(power1-1):
+        octet_C.append(octets2[i])
+
+    IP = ".".join(octet_C)
+    print(IP)
+
+    print(f"The network block size is {network}; range is {IP}.{network}.0 - {IP}.{network}.255 ")
