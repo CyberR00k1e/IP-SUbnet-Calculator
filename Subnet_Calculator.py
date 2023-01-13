1ba076ed2d74d7b048879813db12f855a3d6a39a
srt <sharat.nautiyal@gmail.com>
2023-01-13 11:01:15 +0800

    Final code block

--- Subnet_Calculator.py
+++ Subnet_Calculator.py
@@ -2,105 +2,122 @@ import math
 import re
 import streamlit as st
 #cidr = "10.10.31.22/27"
-cidr= input("please enter the private CIDR in IP_Address/subnet format: " ).strip()
-new= cidr.split("/")
-octets1=new[0]
-octets2=octets1.split(".")
-#print(octets2)
+#cidr= input("please enter the private CIDR in IP_Address/subnet format: " ).strip()
+def subnet(cidr):
+    h=[]
+    new= cidr.split("/")
+    octets1=new[0]
+    octets2=octets1.split(".")
+    #print(octets2)
 
-class_A= re.search("10.*.*,*",new[0])
-class_B=re.search("(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)", new[0])
-class_C=re.search("192.168.*.*", new[0])
+    class_A= re.search("10.*.*,*",new[0])
+    class_B=re.search("(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)", new[0])
+    class_C=re.search("192.168.*.*", new[0])
 
-if class_A:
-    block=8
-elif class_B:
-    block=16
-elif class_C:
-    block=24
-
-subnets= int(new[1])-block
-hosts= 32-int(new[1])
-#print(hosts, subnets)
-#test=new[1]/8 !=0 then div = test.split(".") and power = new[1]-(8*div), z2^power is th
-octet_used1=int(new[1])/8
-#print(octet_used1)
-#if octet_used1 !=0:
-if octet_used1 != 1:
-
-    octet_used2=str(octet_used1)
-    final_octet= octet_used2.split(".")
-    power1=int(final_octet[0])
-
-    power2=int(new[1])-(8*power1)
-
-    power3 = 8 - power2
-
-    network = math.pow(2, power3)
-    network = int(network)
-    print(f'''
-    ******************************
-    Network block size is {network}"
-    ******************************''')
-    if (int(new[1])>8 and int(new[1])< 16):
-            octet_A=[]
-            for i in range(1):
-                octet_A.append(octets2[i])
-
-            IP = ".".join(octet_A)
-            #print(IP)
-            print(f"The network blocks are: ")
-            for i in range(0,256,network):
-
-                print(f"subnet is {IP}.{i}.0")
-
-    elif (int(new[1])>16 and int(new[1])< 24):
-            octet_B=[]
-            for i in range(2):
-
-                octet_B.append(octets2[i])
-            print(octet_B)
-
-            IP = ".".join(octet_B)
-
-            print(f"The network blocks are: ")
-
-            for i in range(0,256,network):
-
-                print(f"subnet is {IP}.{i}.0")
-
-
-    elif (int(new[1]) >24 and int(new[1]) < 32):
-        octet_C = []
-        for i in range(3):
-            octet_C.append(octets2[i])
-        print(octet_C)
-
-        IP = ".".join(octet_C)
-
-        print(f"The network blocks are: ")
-        for i in range(0, 256, network):
-            print(f"subnet is {IP}.{i}")
-
-else :
-
-    octet_used1=int(octet_used1)
-    octets3=[]
-    for i in range(octet_used1):
-        octets3.append(octets2[i])
-        #print(octets3)
-        network = ".".join(octets3)
     if class_A:
-        print(f"Subnet is 0; Network is {network}.0.0.0")
-    if class_B:
+        block=8
+    elif class_B:
+        block=16
+    elif class_C:
+        block=24
+
+    subnets= int(new[1])-block
+    hosts= 32-int(new[1])
+    #print(hosts, subnets)
+    #test=new[1]/8 !=0 then div = test.split(".") and power = new[1]-(8*div), z2^power is th
+    octet_used1=int(new[1])/8
+    #print(octet_used1)
+    #if octet_used1 !=0:
+    if octet_used1 != 1:
+
+        octet_used2=str(octet_used1)
+        final_octet= octet_used2.split(".")
+        power1=int(final_octet[0])
+
+        power2=int(new[1])-(8*power1)
+
+        power3 = 8 - power2
+
+        network = math.pow(2, power3)
+        network = int(network)
+        a=network
+        print(f'''
+        ******************************
+        Network block size is {network}"
+        ******************************''')
+
+
+        if (int(new[1])>8 and int(new[1])< 16):
+                octet_A=[]
+                for i in range(1):
+                    octet_A.append(octets2[i])
+
+                IP = ".".join(octet_A)
+                #print(IP)
+                print(f"The network blocks are: ")
+
+
+                for i in range(0,256,network):
+                    print(f"subnet is {IP}.{i}.0")
+                    h.append(f"{IP}.{i}.0")
+
+
+
+        elif (int(new[1])>16 and int(new[1])< 24):
+                octet_B=[]
+                for i in range(2):
+
+                    octet_B.append(octets2[i])
+                print(octet_B)
+
+                IP = ".".join(octet_B)
+
+                print(f"The network blocks are: ")
+
+                for i in range(0,256,network):
+
+                    print(f"subnet is {IP}.{i}.0")
+                    h.append(f"{IP}.{i}.0")
+
+
+        elif (int(new[1]) >24 and int(new[1]) < 32):
+            octet_C = []
+            for i in range(3):
+                octet_C.append(octets2[i])
+            print(octet_C)
+
+            IP = ".".join(octet_C)
+
+            print(f"The network blocks are: ")
+
+            for i in range(0, 256, network):
+                print(f"subnet is {IP}.{i}")
+                h.append(f"{IP}.{i}")
+
+    else :
+
+        octet_used1=int(octet_used1)
+        octets3=[]
+        for i in range(octet_used1):
+            octets3.append(octets2[i])
+            #print(octets3)
+            network = ".".join(octets3)
+        if class_A:
+            print(f"Subnet is 0; Network is {network}.0.0.0")
+            i="Subnet is 0; Network is {network}.0.0.0"
+        if class_B:
             print(f"Subnet is 0; Network is {network}.0.0")
-    if class_C:
-        print(f"Subnet is 0; Network is {network}.0")
+            i=f"Subnet is 0; Network is {network}.0.0"
+        if class_C:
+            print(f"Subnet is 0; Network is {network}.0")
+            i=f"Subnet is 0; Network is {network}.0"
 
-        #print(IP)
+            #print(IP)
 
-    #print(f"The subnet is 0; the network is {IP}.0.0")
+        #print(f"The subnet is 0; the network is {IP}.0.0")
 
+
+    return h,i,a
 
 
 
--- frontend.py
+++ frontend.py
@@ -1,108 +1,25 @@
-
 import streamlit as st
-cidr = st.text_input(label="input")
-import math
-import re
-import streamlit as st
-#cidr = "10.10.31.22/27"
-cidr= input("please enter the private CIDR in IP_Address/subnet format: " ).strip()
-new= cidr.split("/")
-octets1=new[0]
-octets2=octets1.split(".")
-#print(octets2)
+import Subnet_Calculator as sc
+st.title("Subnet Calculator")
+st.write("Easily get the network blocks calculated by the app")
 
-class_A= re.search("10.*.*,*",new[0])
-class_B=re.search("(^172\.1[6-9]\.)|(^172\.2[0-9]\.)|(^172\.3[0-1]\.)", new[0])
-class_C=re.search("192.168.*.*", new[0])
+user_input=st.text_input(label= "Input the CIDR", placeholder="enter the subnet in CIDR format")
+button=st.button(label="button", key="button")
 
-if class_A:
-    block=8
-elif class_B:
-    block=16
-elif class_C:
-    block=24
+if button:
+    output = (sc.subnet(user_input))
 
-subnets= int(new[1])-block
-hosts= 32-int(new[1])
-#print(hosts, subnets)
-#test=new[1]/8 !=0 then div = test.split(".") and power = new[1]-(8*div), z2^power is th
-octet_used1=int(new[1])/8
-#print(octet_used1)
-#if octet_used1 !=0:
-if octet_used1 != 1:
+    output=list(output)
+    hh=f'''
+    ****************************************
+    The Network Block Size is **{output[2]}**
+    ****************************************
+    '''
 
-    octet_used2=str(octet_used1)
-    final_octet= octet_used2.split(".")
-    power1=int(final_octet[0])
-
-    power2=int(new[1])-(8*power1)
-
-    power3 = 8 - power2
-
-    network = math.pow(2, power3)
-    network = int(network)
-    print(f'''
-    ******************************
-    Network block size is {network}"
-    ******************************''')
-    if (int(new[1])>8 and int(new[1])< 16):
-            octet_A=[]
-            for i in range(1):
-                octet_A.append(octets2[i])
-
-            IP = ".".join(octet_A)
-            #print(IP)
-            print(f"The network blocks are: ")
-            for i in range(0,256,network):
-
-                print(f"subnet is {IP}.{i}.0")
-
-    elif (int(new[1])>16 and int(new[1])< 24):
-            octet_B=[]
-            for i in range(2):
-
-                octet_B.append(octets2[i])
-
-
-            IP = ".".join(octet_B)
-
-            print(f"The network blocks are: ")
-
-            for i in range(0,256,network):
-
-                print(f"subnet is {IP}.{i}.0")
-
-
-    elif (int(new[1]) >24 and int(new[1]) < 32):
-        octet_C = []
-        for i in range(3):
-            octet_C.append(octets2[i])
-
-
-        IP = ".".join(octet_C)
-
-        print(f"The network blocks are: ")
-        for i in range(0, 256, network):
-            print(f"subnet is {IP}.{i}")
-
-else :
-
-    octet_used1=int(octet_used1)
-    octets3=[]
-    for i in range(octet_used1):
-        octets3.append(octets2[i])
-        #print(octets3)
-        network = ".".join(octets3)
-    if class_A:
-        print(f"Subnet is 0; Network is {network}.0.0.0")
-    if class_B:
-            print(f"Subnet is 0; Network is {network}.0.0")
-    if class_C:
-        print(f"Subnet is 0; Network is {network}.0")
-
-        #print(IP)
-
-    #print(f"The subnet is 0; the network is {IP}.0.0")
+    st.markdown(hh)
+    st.markdown(":red[The Network Blocks are]")
+    for i in output[0]:
+        st.write(i)
 
 
 
