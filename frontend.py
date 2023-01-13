bec6a974e1c1fb568592c3c0c419a7f7ad2b5419
srt <sharat.nautiyal@gmail.com>
2023-01-13 11:12:54 +0800

    - fina fina code block

--- frontend.py
+++ frontend.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import Subnet_Calculator as sc
 st.title("Subnet Calculator")
-st.write("Easily get the network blocks calculated by the app")
+st.markdown(":blue[Easily get the network blocks calculated by the app]")
 
 user_input=st.text_input(label= "Input the CIDR", placeholder="enter the subnet in CIDR format")
 button=st.button(label="button", key="button")
@@ -12,12 +12,13 @@ if button:
     output=list(output)
     hh=f'''
     ****************************************
-    The Network Block Size is **{output[2]}**
+    The Network Block Size is :red[**{output[2]}**]
     ****************************************
     '''
 
     st.markdown(hh)
     st.markdown(":red[The Network Blocks are]")
+
     for i in output[0]:
         st.write(i)
 
